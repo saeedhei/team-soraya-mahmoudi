48b7ccbefe1e7811b3350a4b0b929a188f949eea
Soraya1987 <101457380+Soraya1987@users.noreply.github.com>
2025-04-25 16:25:49 +0330

    create home page and update App.tsx

---
+++ frontend-medic/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white text-blue-900">
+      <h1 className="text-3xl font-bold text-center mt-30">welcom to MedicApp</h1>
+    </div>
+  );
+}
