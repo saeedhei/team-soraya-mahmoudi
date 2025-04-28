62f8f13b56932181a99d7752d8e132fbf9a33b20
Soraya1987 <101457380+Soraya1987@users.noreply.github.com>
2025-04-29 00:05:58 +0330

    feat: implement user signup with password hashing and JWT token generation

--- backend-medic/src/graphql/resolvers/userResolvers.ts
+++ backend-medic/src/graphql/resolvers/userResolvers.ts
@@ -1,12 +1,37 @@
+import { User } from '../../domain/users/models/user.model'; 
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+const SECRET = process.env.JWT_SECRET || 'myverysecretkey';
 
 export const userResolvers= {
     Query: {
-        hello:()=> 'Hello from user resolvers!'
+        me: async (_: any, __: any, context: any) => {
+            if (!context.user) throw new Error('Not authenticated');
+            return context.user;
+          },
     },
     Mutation:{
         signup: async (_:any, args:{username:string, email:string, password:string})=>{
             const { username,email,password}= args;
-            return "Signup Mutation Ready!"
-        },
+            
+
+     // ۱. هش کردن پسورد
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // ۲. ساخت کاربر جدید
+      const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+      
+      const token = jwt.sign({ id: user.id }, SECRET, { expiresIn: '7d' });
+
+      // ۳. برگردوندن نتیجه
+      return {
+        token,
+        user,
+      };
     },
+  },
 };
