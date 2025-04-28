6126d9138186ed25830b85f3466911fe669de564
Soraya1987 <101457380+Soraya1987@users.noreply.github.com>
2025-04-28 18:19:20 +0330

    feat: add username, email, and password fields to User model

--- backend-medic/src/domain/users/models/user.model.ts
+++ backend-medic/src/domain/users/models/user.model.ts
@@ -1,5 +1,11 @@
 import { Schema,model } from "mongoose";
 
-const userSchema=new Schema({},{timestamps:true});
+const userSchema=new Schema({
+    username:{type:String, required:true, unique:true},
+    email:{type:String, required:true, unique:true, lowercase:true, trim: true},
+    password:{type:String, required:true},
+},
+{timestamps:true}
+);
 
 export const user= model('User',userSchema);
