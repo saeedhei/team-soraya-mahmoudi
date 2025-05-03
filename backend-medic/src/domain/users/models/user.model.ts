d7790d5581bd5cfc7465eb6421c34d1f83fe6916
Soraya1987 <101457380+Soraya1987@users.noreply.github.com>
2025-05-03 05:19:08 +0330

    feat(user-model): add isVerified field to track account verification status

--- backend-medic/src/domain/users/models/user.model.ts
+++ backend-medic/src/domain/users/models/user.model.ts
@@ -4,6 +4,7 @@ const userSchema=new Schema({
     username:{type:String, required:true, unique:true},
     email:{type:String, required:true, unique:true, lowercase:true, trim: true},
     password:{type:String, required:true},
+    isVerified:{type:Boolean, default:false},
 },
 {timestamps:true}
 );
